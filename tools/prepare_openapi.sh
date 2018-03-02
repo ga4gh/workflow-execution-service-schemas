c8e3980cc33542877ad062bf6f77bb7400237db1
David Steinberg <david@resium.com>
2018-03-02 14:18:33 -0800

    Change ProtobufStruct to WesObject Remove unused tool Add swagger yaml to gitignore

--- tools/prepare_openapi.sh
+++
@@ -1,6 +0,0 @@
-# Add a line for setting the controller "app" which matches the name of the
-# module that hosts the controller functions.
-sed -i 's/"operation/"x-swagger-router-controller": "ga4gh.wes.server", \n        "operation/g' workflow_execution_service.swagger.json
-# Inject a base path
-sed -i 's/"swagger"/  "basePath": "\/", "swagger"/g' workflow_execution_service.swagger.json
-mv workflow_execution_service.swagger.json swagger/workflow_execution_service.swagger.json
