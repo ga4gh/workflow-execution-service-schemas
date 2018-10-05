426397498e882ec430bec102e3536855aebe62d0
James Eddy <james.a.eddy@gmail.com>
2018-10-05 11:40:20 +0200

    try again; cache didn't work, so add separate deploy for docs

---
+++ scripts/deploydocs.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -e
+set -v
+
+if [ "$TRAVIS_BRANCH" == "master" ]; then
+    cp docs/html5/index.html docs/
+elif [ "$TRAVIS_BRANCH" !=  "gh-pages" ]; then
+  branch=$(echo "$TRAVIS_BRANCH" | awk '{print tolower($0)}')
+  branchpath="preview/$branch"
+  mkdir -p "$branchpath/docs"
+  cp docs/html5/index.html "$branchpath/docs/"
+fi
+
