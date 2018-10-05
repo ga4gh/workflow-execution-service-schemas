31ed28d789676c880665858b47114840d0517c7c
James Eddy <james.a.eddy@gmail.com>
2018-10-05 14:31:42 +0200

    fully separate docs and ui preview paths

--- scripts/deploydocs.sh
+++ scripts/deploydocs.sh
@@ -7,7 +7,7 @@ if [ "$TRAVIS_BRANCH" == "master" ]; then
     cp docs/html5/index.html docs/
 elif [ "$TRAVIS_BRANCH" !=  "gh-pages" ]; then
   branch=$(echo "$TRAVIS_BRANCH" | awk '{print tolower($0)}')
-  branchpath="preview/$branch"
+  branchpath="preview-docs/$branch"
   mkdir -p "$branchpath/docs"
   cp docs/html5/index.html "$branchpath/docs/"
 fi
--- scripts/deployui.js
+++ scripts/deployui.js
@@ -10,12 +10,10 @@ set('-v');
 var branch = process.env.TRAVIS_BRANCH && process.env.TRAVIS_BRANCH.toLowerCase();
 if (branch && branch == 'master') {
     cp('-R', 'web_deploy/*', './');
-    cp('docs/html5/index.html', 'docs/');
 } else if (branch && branch !== 'gh-pages') {
-  var branchPath = path.join('preview', branch, '/');
+  var branchPath = path.join('preview-ui', branch, '/');
   var branchDocsPath = path.join(branchPath, 'docs', '/');
   mkdir('-p', branchPath);
   mkdir('-p', branchDocsPath);
   cp('-R', 'web_deploy/*', branchPath);
-  cp('docs/html5/index.html', branchDocsPath);
 }
