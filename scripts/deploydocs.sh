58c1a530b6494f4451510d4e931b1c49cdf8c625
James Eddy <james.a.eddy@gmail.com>
2018-10-07 12:23:46 +0200

    just use branch name for preview, not PR branch

--- scripts/deploydocs.sh
+++ scripts/deploydocs.sh
@@ -3,17 +3,11 @@
 set -e
 set -v
 
-if [ "$TRAVIS_PULL_REQUEST" ]; then
-  BRANCH="$TRAVIS_PULL_REQUEST_BRANCH"
-else
-  BRANCH="$TRAVIS_BRANCH"
-fi
-
-if [ "$BRANCH" == "master" ]; then
+if [ "$TRAVIS_BRANCH" == "master" ]; then
     cp docs/html5/index.html docs/
     cp openapi/workflow_execution_service.swagger.yaml ./swagger.yaml
-elif [ "$BRANCH" !=  "gh-pages" ]; then
-  branch=$(echo "$BRANCH" | awk '{print tolower($0)}')
+elif [ "$TRAVIS_BRANCH" !=  "gh-pages" ]; then
+  branch=$(echo "$TRAVIS_BRANCH" | awk '{print tolower($0)}')
   branchpath="preview/$branch"
   mkdir -p "$branchpath/docs"
   cp docs/html5/index.html "$branchpath/docs/"
