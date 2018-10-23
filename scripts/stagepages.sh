81a36e63fc562bc3147883e2e2a518b6199813c9
James Eddy <james.a.eddy@gmail.com>
2018-10-22 22:10:34 -0700

    remove option to preview PR pages

--- scripts/stagepages.sh
+++ scripts/stagepages.sh
@@ -3,18 +3,12 @@
 set -e
 set -v
 
-if [ "$TRAVIS_PULL_REQUEST" != false ]; then
-  branch="pull-$TRAVIS_PULL_REQUEST"
-else
-  branch=$(echo "$TRAVIS_BRANCH" | awk '{print tolower($0)}')
-fi
-
-
-if [ "$TRAVIS_BRANCH" == "master" ] || [ "$TRAVIS_BRANCH" == "develop" ]; then
+if [ "$TRAVIS_BRANCH" == "develop" ]; then
     cp docs/html5/index.html docs/
     cp openapi/workflow_execution_service.swagger.yaml ./swagger.yaml
     cp -R web_deploy/* .
-elif [ "$TRAVIS_BRANCH" != "gh-pages" ]; then
+elif [ "$TRAVIS_BRANCH" != "master" ] && [ "$TRAVIS_BRANCH" != "gh-pages" ]; then
+  branch=$(echo "$TRAVIS_BRANCH" | awk '{print tolower($0)}')
   branchpath="preview/$branch"
   mkdir -p "$branchpath/docs"
   cp docs/html5/index.html "$branchpath/docs/"
