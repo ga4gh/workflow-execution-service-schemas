4042f1423a50b04abb3c32ac92496a586e834c64
James Eddy <james.a.eddy@gmail.com>
2019-05-03 14:39:34 -0700

    Include latest docs build config from other Cloud WS repos
    Update 'scripts' for fetching and creating GH pages to match
    changes in the Data Repository Service (DRS) API repo. Also
    update Travis config to preserve history on 'gh-pages' branch.
    
    See #126

--- scripts/stagepages.sh
+++ scripts/stagepages.sh
@@ -3,16 +3,22 @@
 set -e
 set -v
 
-if [ "$TRAVIS_BRANCH" == "master" ]; then
-    cp docs/html5/index.html docs/
-    cp openapi/workflow_execution_service.swagger.yaml ./swagger.yaml
-    cp -R web_deploy/* .
-elif [ "$TRAVIS_BRANCH" != "gh-pages" ]; then
-  branch=$(echo "$TRAVIS_BRANCH" | awk '{print tolower($0)}')
-  branchpath="preview/$branch"
+if [ "$TRAVIS_BRANCH" != "gh-pages" ]; then
+  if [ "$TRAVIS_BRANCH" == "master" ]; then
+    branchpath="."
+  else
+    branch=$(echo "$TRAVIS_BRANCH" | awk '{print tolower($0)}')
+    branchpath="preview/$branch"
+  fi  
   mkdir -p "$branchpath/docs"
   cp docs/html5/index.html "$branchpath/docs/"
+  cp docs/pdf/index.pdf "$branchpath/docs/"
+  cp docs/asciidoc/*.png "$branchpath/docs/"
   cp openapi/workflow_execution_service.swagger.yaml "$branchpath/swagger.yaml"
   cp -R web_deploy/* "$branchpath/"
 fi
 
+# do some cleanup
+rm -rf node_modules
+rm -rf web_deploy
+rm -rf spec
